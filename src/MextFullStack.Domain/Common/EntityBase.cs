b229501532fb5c505414017addba497c2338602e
Melike Aydın <melike.ay45@gmail.com>
2024-05-07 00:32:23 +0300

    Add project files.

---
+++ src/MextFullStack.Domain/Common/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace MextFullStack.Domain.Common
+{
+    public class EntityBase<TKey> where TKey : struct
+    {
+        public TKey Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string CreatedByUserId { get; set; } 
+        public DateTime? ModifiedOn { get; set; }
+        public string? ModifiedByUserId { get; set; }
+    }
+}
---
+++ src/MextFullStack.Domain/Entities/Customer.cs
@@ -0,0 +1,27 @@
+﻿using MextFullStack.Domain.Common;
+
+
+namespace MextFullStack.Domain.Entities
+{
+
+    //string referans tür olduğu için burda entitybase deki struct kısıtlamasından dolayı hata veriyor.
+    public class Customer:EntityBase<string>
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public Customer()
+        {
+            CreatedOn = DateTime.Now;
+
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
